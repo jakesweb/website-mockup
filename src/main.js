f2a6b543ba6d8dfab938cd04f15f96a4eab97691
Jacob Colborn <jake@jakesweb.me>
2020-06-11 23:39:43 -0400

    added google places api for address auto-completion

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
+import DefaultLayout from "~/layouts/Default.vue";
 
-export default function (Vue, { router, head, isClient }) {
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component("Layout", DefaultLayout);
+  head.script.push({
+    src:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyC1jH6xj4rmBzOCALb4GfVfoCoSOHmvnDA&libraries=places",
+    body: false,
+  });
 }
--- src/pages/Ship.vue
+++ src/pages/Ship.vue
@@ -18,21 +18,27 @@
         placeholder="Inch"
       />
       <label for="deliver">Deliver</label>
-      <input
-        type="text"
-        name="deliver"
-        id="deliver"
-        placeholder="Delivery Address"
-      />
+      <vue-google-autocomplete
+        ref="address"
+        id="delivery"
+        classname="form-control"
+        placeholder="Please type the address for delivery"
+        v-on:placechanged="getAddressData"
+      ></vue-google-autocomplete>
+      <label for="apt">Apartment, Suite, or Unit</label>
+      <input type="text" name="deliver-apt" id="deliver-apt" />
       <label for="insurance">Insurance</label>
       <input type="checkbox" name="insurance" id="insurance" />
       <label for="pickup">Pickup</label>
-      <input
-        type="text"
-        name="pickup"
+      <vue-google-autocomplete
+        ref="address"
         id="pickup"
-        placeholder="Pickup Address"
-      />
+        classname="form-control"
+        placeholder="Please type the address for delivery"
+        v-on:placechanged="getAddressData"
+      ></vue-google-autocomplete>
+      <label for="apt">Apartment, Suite, or Unit</label>
+      <input type="text" name="pickup-apt" id="pickup-apt" />
       <label for="pickup-time">Pickup Time</label>
       <input type="date" name="pickup-time" id="pickup-time" />
     </form>
@@ -40,9 +46,35 @@
 </template>
 
 <script>
+import VueGoogleAutocomplete from "vue-google-autocomplete";
+
 export default {
+  components: { VueGoogleAutocomplete },
   metaInfo: {
     title: "Shipping",
   },
+  data: function() {
+    return {
+      address: "",
+    };
+  },
+  mounted() {
+    this.$refs.address.focus();
+  },
+  methods: {
+    // this.address - country, locality, postal_code, street_number + route, administrative_area_level_1
+    getAddressData: function(addressData, placeResultData, id) {
+      this.address = addressData;
+      console.log(this.address);
+      console.log(placeResultData);
+    },
+  },
 };
 </script>
+
+<style>
+input {
+  display: block;
+  width: 100%;
+}
+</style>
